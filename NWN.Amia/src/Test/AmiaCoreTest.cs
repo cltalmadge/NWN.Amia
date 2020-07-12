674a4c9727d7b422219df864302abeb89235f2dc
Caelan <talmadge12@gmail.com>
2020-07-12 11:35:59 -0400

    Added simple tests to AmiaCoreTest.cs

---
+++ NWN.Amia/src/Test/AmiaCoreTest.cs
@@ -0,0 +1,27 @@
+﻿using NWN.Amia.Main.Core;
+using Xunit;
+
+namespace NWN.Amia.test
+{
+    public class AmiaCoreTest
+    {
+        private readonly AmiaCore _amiaCore;
+
+        public AmiaCoreTest()
+        {
+            _amiaCore = new AmiaCore();
+        }
+
+        [Fact]
+        public void OnRunScriptHandlesScripts()
+        {
+            Assert.Equal(0, _amiaCore.OnRunScript("z_real_script_name", 0));
+        }
+
+        [Fact]
+        public void OnRunScriptCannotHandleNonExistent()
+        {
+            Assert.Equal(-1, _amiaCore.OnRunScript("", 0));
+        }
+    }
+}
