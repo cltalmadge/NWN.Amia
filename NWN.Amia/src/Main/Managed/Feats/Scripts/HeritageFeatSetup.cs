59ba48ae88cd4bdd9a111c08532efef185f96bf1
Caelan <talmadge12@gmail.com>
2020-07-15 12:31:33 -0400

    Added new managed script: HeritageFeatSetup.cs. Will be responsible for adding bonus attributes and the like.

---
+++ NWN.Amia/src/Main/Managed/Feats/Scripts/HeritageFeatSetup.cs
@@ -0,0 +1,13 @@
+﻿using NWN.Amia.Main.Core.Types;
+
+namespace NWN.Amia.Main.Managed.Feats.Scripts
+{
+    [ScriptName("heritage_setup")]
+    public class HeritageFeatSetup : IRunnableScript
+    {
+        public int Run(uint nwnObjectId)
+        {
+            return 0;
+        }
+    }
+}
