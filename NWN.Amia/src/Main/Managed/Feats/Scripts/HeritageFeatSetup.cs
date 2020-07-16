f4ae472d0f899a24e9bac72e6bec3d67a6de0dcd
Caelan <talmadge12@gmail.com>
2020-07-16 17:46:55 -0400

    Added support for template managed subraces.

--- NWN.Amia/src/Main/Managed/Feats/Scripts/HeritageFeatSetup.cs
+++ NWN.Amia/src/Main/Managed/Feats/Scripts/HeritageFeatSetup.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using NWN.Amia.Main.Core.Types;
 using NWN.Amia.Main.Managed.Races;
 using NWN.Core;
+using NWN.Core.NWNX;
 
 namespace NWN.Amia.Main.Managed.Feats.Scripts
 {
@@ -11,11 +13,12 @@ namespace NWN.Amia.Main.Managed.Feats.Scripts
         public int Run(uint nwnObjectId)
         {
             var playerRace = NWScript.GetRacialType(nwnObjectId);
-
-            if (!GetListOfRacesWithHeritage().Contains(playerRace)) return 0;
-
-         
+            var pckey = NWScript.GetItemPossessedBy(nwnObjectId, "ds_pckey");
             
+            if (!GetListOfRacesWithHeritage().Contains(playerRace)) return 0;
+            if (NWScript.GetLocalInt(pckey, "heritage_setup") == 1) return 0;
+
+            PerformHeritageFeatSetup(nwnObjectId);
                 
             return 0;
         }
@@ -33,6 +36,11 @@ namespace NWN.Amia.Main.Managed.Feats.Scripts
                 (int) ManagedRaces.RacialType.Ogrillon,
                 (int) ManagedRaces.RacialType.Feytouched
             };
+        }
+
+        private void PerformHeritageFeatSetup(in uint nwnObjectId)
+        {
+            NWScript.SendMessageToPC(nwnObjectId, "TODO: Implement heritage abilities.");
         }
     }
 }
--- NWN.Amia/src/Main/Managed/Races/Script/RaceAbilities.cs
+++ NWN.Amia/src/Main/Managed/Races/Script/RaceAbilities.cs
@@ -8,25 +8,52 @@ namespace NWN.Amia.Main.Managed.Races.Script
     [ScriptName("race_abilities")]
     public class RaceAbilities : IRunnableScript
     {
-        public List<Effect> AppliedEffects { get; set; }
-
         public int Run(uint nwnObjectId)
         {
             // Race isn't managed. Do nothing.
-            if (!ManagedRaces.Races.ContainsKey(NWScript.GetRacialType(nwnObjectId)))
+            var playerRace = NWScript.GetSubRace(nwnObjectId) == ""
+                ? NWScript.GetRacialType(nwnObjectId)
+                : GetRaceFromSubrace(nwnObjectId);
+
+            var raceIsNotManaged = !ManagedRaces.Races.ContainsKey(playerRace);
+            if (raceIsNotManaged)
             {
                 return 0;
             }
+            
+            SetEffectsToSupernaturalAndApply(nwnObjectId);
 
-            var raceEffects = new RacialEffectCreator().GetFeatEffects(nwnObjectId);
+            return 0;
+        }
+
+        private void SetEffectsToSupernaturalAndApply(uint nwnObjectId)
+        {
+            var raceEffects = GetListOfEffectsForRace(nwnObjectId);
             var supernaturalEffects = ConvertEffectsToSupernatural(raceEffects);
 
             foreach (Effect effect in supernaturalEffects)
             {
                 NWScript.ApplyEffectToObject(NWScript.DURATION_TYPE_PERMANENT, effect, nwnObjectId);
             }
+        }
 
-            return 0;
+        private int GetRaceFromSubrace(in uint nwnObjectId)
+        {
+            return NWScript.GetSubRace(nwnObjectId).ToLower() switch
+            {
+                "aasimar" => (int) ManagedRaces.RacialType.Aasimar,
+                "tiefling" => (int) ManagedRaces.RacialType.Tiefling,
+                "feytouched" => (int) ManagedRaces.RacialType.Feytouched,
+                "fey'ri" => (int) ManagedRaces.RacialType.Feyri,
+                "feyri" => (int) ManagedRaces.RacialType.Feyri,
+                _ => -1
+            };
+        }
+
+        private static List<Effect> GetListOfEffectsForRace(uint nwnObjectId)
+        {
+            var raceEffects = new RacialEffectCreator().GetFeatEffects(nwnObjectId);
+            return raceEffects;
         }
 
         private List<Effect> ConvertEffectsToSupernatural(List<Effect> raceEffects)
