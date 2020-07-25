3cfd822d2092418ab8b1bf81bf331fdbf5747163
Caelan <talmadge12@gmail.com>
2020-07-24 20:06:59 -0400

    Added UsedImplicitly to MockScript.cs to make sure classes can be resolved with multiple attributes (testing usage of reflection to resolve classes for scripts).

--- NWN.Amia/src/Test/Mock/MockScript.cs
+++ NWN.Amia/src/Test/Mock/MockScript.cs
@@ -1,8 +1,9 @@
-﻿using NWN.Amia.Main.Core.Types;
+﻿using JetBrains.Annotations;
+using NWN.Amia.Main.Core.Types;
 
 namespace NWN.Amia.Test.Mock
 {
-    [ScriptName("z_real_script_name")]
+    [ScriptName("z_real_script_name"), UsedImplicitly]
     public class MockScript : IRunnableScript
     {
         public int Run(uint nwnObjectId)
