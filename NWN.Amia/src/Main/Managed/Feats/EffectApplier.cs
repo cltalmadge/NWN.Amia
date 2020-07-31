64ba8fe6e7cd220b624e3eb651204690c016c7a2
Caelan <talmadge12@gmail.com>
2020-07-31 11:25:30 -0400

    Cleaned up subrace effects. Moved EffectApplier to its own file.

---
+++ NWN.Amia/src/Main/Managed/Feats/EffectApplier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NWN.Core;
+
+namespace NWN.Amia.Main.Managed.Feats
+{
+    public static class EffectApplier
+    {
+        public static void Apply(uint nwnObjectId, List<Effect> effects)
+        {
+            var supernaturalEffects = MakeEffectsSupernatural(effects);
+
+            foreach (var effect in supernaturalEffects)
+            {
+                NWScript.ApplyEffectToObject(NWScript.DURATION_TYPE_PERMANENT, effect, nwnObjectId);
+            }
+        }
+
+        private static IEnumerable<Effect> MakeEffectsSupernatural(List<Effect> effects) => effects
+            .Select(effect => NWScript.SupernaturalEffect(effect)).Select(dummy => (Effect) dummy).ToList();
+    }
+}
