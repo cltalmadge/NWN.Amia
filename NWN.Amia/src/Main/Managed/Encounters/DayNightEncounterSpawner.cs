c054a393182a5c398daa5fc3008e8bbab365c004
Caelan <talmadge12@gmail.com>
2020-08-15 15:59:21 -0400

    Added debug lines

--- NWN.Amia/src/Main/Managed/Encounters/DayNightEncounterSpawner.cs
+++ NWN.Amia/src/Main/Managed/Encounters/DayNightEncounterSpawner.cs
@@ -35,15 +35,20 @@ namespace NWN.Amia.Main.Managed.Encounters
         public void SpawnEncounters()
         {
             _waypointLocation = GetLocationForSpawn();
+            Console.WriteLine($"Location {_waypointLocation}");
 
             var isNightTime = NWScript.GetTimeHour() < 6 && NWScript.GetTimeHour() >= 18;
-
+            Console.WriteLine($"Time is night {isNightTime}");
+            
             var spawnsVary = NWScript.GetLocalInt(_trigger, "spawns_vary") == 1;
+            Console.WriteLine($"Spawns vary {spawnsVary}");
 
             var spawnsToChoose = isNightTime && spawnsVary ? VarPrefixes[1] : VarPrefixes[0];
+            Console.WriteLine($"Spawns chosen {spawnsToChoose}");
 
             var dayCreatureResRefs = GetResRefsForPrefix(spawnsToChoose) as string[] ??
                                      GetResRefsForPrefix(spawnsToChoose).ToArray();
+            Console.WriteLine($"Creatures to spawn {dayCreatureResRefs}");
 
             var numToSpawn = NWScript.d3() + 4;
             var maxSpawns = DoubleSpawn ? numToSpawn * 2 : numToSpawn;
@@ -82,6 +87,7 @@ namespace NWN.Amia.Main.Managed.Encounters
             for (var i = 0; i < maxSpawns; i++)
             {
                 var randomCreature = new Random().Next(0, resRefs.Count);
+                Console.WriteLine($"Creature to spawn {resRefs[randomCreature]}");
                 SpawnEncounterAtWaypoint(resRefs[randomCreature]);
             }
         }
