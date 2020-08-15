bb95059571dc585efdf7e43ad8be4786db7b252f
Caelan <talmadge12@gmail.com>
2020-08-15 16:20:33 -0400

    Removed log lines

--- NWN.Amia/src/Main/Managed/Encounters/DayNightEncounterSpawner.cs
+++ NWN.Amia/src/Main/Managed/Encounters/DayNightEncounterSpawner.cs
@@ -16,13 +16,6 @@ namespace NWN.Amia.Main.Managed.Encounters
         private readonly uint _player;
         private readonly uint _trigger;
 
-
-        public DayNightEncounterSpawner(uint trigger, uint objectWithVariables)
-        {
-            _trigger = trigger;
-            _objectWithVariables = objectWithVariables;
-        }
-
         public DayNightEncounterSpawner(uint trigger, uint objectWithVariables, uint player)
         {
             _trigger = trigger;
@@ -35,20 +28,15 @@ namespace NWN.Amia.Main.Managed.Encounters
         public void SpawnEncounters()
         {
             _waypointLocation = GetLocationForSpawn();
-            Console.WriteLine($"Location {_waypointLocation}");
 
             var isNightTime = NWScript.GetTimeHour() < 6 && NWScript.GetTimeHour() >= 18;
-            Console.WriteLine($"Time is night {isNightTime}");
-            
+
             var spawnsVary = NWScript.GetLocalInt(_trigger, "spawns_vary") == 1;
-            Console.WriteLine($"Spawns vary {spawnsVary}");
 
             var spawnsToChoose = isNightTime && spawnsVary ? VarPrefixes[1] : VarPrefixes[0];
-            Console.WriteLine($"Spawns chosen {spawnsToChoose}");
 
             var dayCreatureResRefs = GetResRefsForPrefix(spawnsToChoose) as string[] ??
                                      GetResRefsForPrefix(spawnsToChoose).ToArray();
-            Console.WriteLine($"Creatures to spawn {dayCreatureResRefs}");
 
             var numToSpawn = NWScript.d3() + 4;
             var maxSpawns = DoubleSpawn ? numToSpawn * 2 : numToSpawn;
@@ -58,10 +46,8 @@ namespace NWN.Amia.Main.Managed.Encounters
         private IntPtr GetLocationForSpawn()
         {
             var playerLocation = NWScript.GetPosition(_player);
-            // return NWScript.GetLocation(NWScript.GetNearestObjectByTag("ds_spwn", _trigger));
             return NWScript.Location(NWScript.GetArea(_player),
-                GetPositionInFront(playerLocation, 15, NWScript.GetFacing(_player)),
-                NWScript.GetFacing(_player));
+                GetPositionInFront(playerLocation, 15, NWScript.GetFacing(_player)), NWScript.GetFacing(_player));
         }
 
         private static Vector3 GetPositionInFront(Vector3 original, float distance, float angle) =>
@@ -75,28 +61,22 @@ namespace NWN.Amia.Main.Managed.Encounters
         private static double ChangeInX(float distance, float angle) => distance * Math.Cos(angle);
 
         private static double ChangeInY(float distance, float angle) => distance * Math.Sin(angle);
-        
+
         private static void SpawnCreaturesFromResRefs(int maxSpawns, IReadOnlyList<string> resRefs)
         {
-            if (!resRefs.Any())
-            {
-                Console.WriteLine("Shit broke.");
-                return;
-            }
+            if (!resRefs.Any()) return;
 
             for (var i = 0; i < maxSpawns; i++)
             {
                 var randomCreature = new Random().Next(0, resRefs.Count);
-                Console.WriteLine($"Creature to spawn {resRefs[randomCreature]}");
                 SpawnEncounterAtWaypoint(resRefs[randomCreature]);
-                NWScript.ApplyEffectAtLocation(NWScript.DURATION_TYPE_INSTANT, NWScript.EffectVisualEffect(22), _waypointLocation);
             }
         }
 
         private static void SpawnEncounterAtWaypoint(string resRef)
         {
             var creature = NWScript.CreateObject(NWScript.OBJECT_TYPE_CREATURE, resRef, _waypointLocation);
-            if(creature == NWScript.OBJECT_INVALID) Console.WriteLine("Spawn wasn't valid!");
+            if (creature == NWScript.OBJECT_INVALID) Console.WriteLine("Spawn wasn't valid!");
         }
 
         private static IEnumerable<string> GetResRefsForPrefix(string prefix)
