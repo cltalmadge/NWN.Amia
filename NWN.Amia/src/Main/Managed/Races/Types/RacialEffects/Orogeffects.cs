241901209041d46dff53806eccc1e1d58b00bd8e
Caelan <talmadge12@gmail.com>
2020-08-15 23:02:04 -0400

    Added save decrease to Orog

--- NWN.Amia/src/Main/Managed/Races/Types/RacialEffects/Orogeffects.cs
+++ NWN.Amia/src/Main/Managed/Races/Types/RacialEffects/Orogeffects.cs
@@ -6,15 +6,38 @@ namespace NWN.Amia.Main.Managed.Races.Types.RacialEffects
 {
     public class OrogEffects : IEffectCollector
     {
+        private uint _oid;
+        private bool _hasHeritageFeat;
+        private const int Heritage = 1238;
         public List<Effect> GatherEffectsForObject(uint objectId)
         {
-            return new List<Effect>
+            _oid = objectId;
+            _hasHeritageFeat = HasHeritageFeat();
+            
+            var orogEffects = new List<Effect>
             {
                 NWScript.EffectSkillIncrease(NWScript.SKILL_CRAFT_ARMOR, 2),
                 NWScript.EffectSkillIncrease(NWScript.SKILL_CRAFT_WEAPON, 2),
                 NWScript.EffectDamageResistance(NWScript.DAMAGE_TYPE_FIRE, 5),
                 NWScript.EffectDamageResistance(NWScript.DAMAGE_TYPE_COLD, 5)
             };
+            
+            AddHeritageEffectsIfObjectHasFeat(orogEffects);
+            
+            return orogEffects;
         }
+        
+        private bool HasHeritageFeat()
+        {
+            return NWScript.GetHasFeat(Heritage, _oid) == 1;
+        }
+
+        private void AddHeritageEffectsIfObjectHasFeat(List<Effect> effectsForObject)
+        {
+            if (!_hasHeritageFeat) return;
+
+            effectsForObject.Add(NWScript.EffectSavingThrowDecrease(NWScript.SAVING_THROW_ALL, 1));
+        }
+        
     }
 }
