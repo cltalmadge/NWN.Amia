d98ac043ad3dab8b85b1179e4fbc2704092ab990
Caelan <talmadge12@gmail.com>
2020-08-17 11:29:30 -0400

    Offsetting NWNX Devs' ratty ass design, perhaps?

--- NWN.Amia/src/Main/Managed/Races/Utils/RaceAdjustmentUtils.cs
+++ NWN.Amia/src/Main/Managed/Races/Utils/RaceAdjustmentUtils.cs
@@ -79,10 +79,10 @@ namespace NWN.Amia.Main.Managed.Races.Utils
             {
                 StrBonus = 0,
                 DexBonus = 0,
-                ConBonus = 0,
+                ConBonus = 2,
                 WisBonus = 0,
                 IntBonus = 0,
-                ChaBonus = 0
+                ChaBonus = 2
             };
         }
 
