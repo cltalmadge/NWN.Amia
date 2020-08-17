d0e57329f14b14d494e9d735abf25a68382595b3
Caelan <talmadge12@gmail.com>
2020-08-17 12:12:26 -0400

    Try doing nothing for gnomes.

--- NWN.Amia/src/Main/Managed/Races/Utils/RaceAdjustmentUtils.cs
+++ NWN.Amia/src/Main/Managed/Races/Utils/RaceAdjustmentUtils.cs
@@ -64,9 +64,9 @@ namespace NWN.Amia.Main.Managed.Races.Utils
         {
             return new RacialTemplate
             {
-                StrBonus = 2,
+                StrBonus = 0,
                 DexBonus = 0,
-                ConBonus = -2,
+                ConBonus = 0,
                 WisBonus = 0,
                 IntBonus = 0,
                 ChaBonus = 0
